b0f0031adf55327591057ae401f8038f1c935868
Rodrigo Herrada <jahirrodrigoherrada@hotmail.com>
2021-01-05 15:55:29 -0500

    Cambios subidos

--- script_cepal.bat
+++ script_cepal.bat
@@ -1,3 +1,7 @@
 
-cd scripts
-python lectura_de_archivos.py
+::cd scripts
+::python lectura_de_archivos.py
+
+
+sqlplus CEPAL_OWN/CEPAL_OWN@XE @output\script.sql
+rem #sqlplus CEPAL_OWN/CEPAL_OWN@XE commit;
